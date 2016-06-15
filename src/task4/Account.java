2eb8a1e5aba6cb7f20b3d2133e143a09e6cb2f8d
a5095153 <a5095153@RI100712.adwin.renesas.com>
2016-06-15 09:31:26 +0900

    finish task

--- src/task4/Account.java
+++ src/task4/Account.java
@@ -1,10 +1,15 @@
 package task4;
 
-public class Account {
+class Account {
 	
 	public static void main(String[] args) {
+		Operation op = new Operation();
+		op.setBalance(100);	// 帐户初始金额为100元
 		
-
+		for (int i = 1; i < 1000; i++) {	// 执行1000次存取操作
+			new Thread(op).start();
+		}
+        op.getBalance();
 	}
 
 }
--- src/task4/Operation.java
+++ src/task4/Operation.java
@@ -1,20 +1,42 @@
 package task4;
 
-class Operation {
-	int balance = 100;	// 帐户初始金额为100元
-	int money = 1;
-	public int deposit(int money){
-		while (money >= 1) {
-			this.balance += money;
+class Operation implements Runnable{
+
+	private int balance;
+    int money = 1;	// 单次存取额度为1元
+	public void deposit(int money){
+		if (money >= 1) {
+			balance += money;
+		} else {
+			System.out.println("存入金额需为1的整数倍。");
 		}
-		return balance;
+		System.out.printf("\n向账户存了%d元，新余额为%d元。",money,balance);
 	}
 
-	public int withdrawal(int money) {
-		while (money >= 1) {
-			this.balance -= money;
+	public void withdrawal(int money) {
+		if (money >= 1 && money < balance) {
+			balance -= money;
+		} else {
+			System.out.println("余额不足或输入不正确！");
 		}
-		return balance;
+		System.out.printf("\n从账户取了%d元，账户还有%d元。", money,balance);
+	}
+
+	public void getBalance() {
+		System.out.printf("\n账户最新余额为：%d元。",balance);
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public void run() {
+		// 同步代码块同步线程执行存取方法
+		synchronized (this) {
+			deposit(money);
+			withdrawal(money);
+		}
 	}
 
 }

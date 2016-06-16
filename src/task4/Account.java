51fbc3624ee733440aaa453b1defed80c068b52b
Rusher <rockbee@gmail.com>
2016-06-16 11:21:18 +0900

    del

--- src/task4/Account.java
+++ src/task4/Account.java
@@ -8,23 +8,8 @@ class Account {
 		this.balance = i;
 	}
 
-//	public static void main(String[] args) {
-//		Operation op = new Operation();
-//		op.setBalance(100);	// 帐户初始金额为100元
-//		
-//		for (int i = 1; i < 1000; i++) {	// 执行1000次存取操作
-//			new Thread(op).start();
-//		}
-//		try {
-//			Thread.sleep(1000);
-//			op.getBalance();
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
 	public synchronized void deposit() {
-		int money = 1; //设置存款额度为1元
+		int money = 1; //设置存款初始额度为1元
 		if (money >= 1) {
 			balance += money;
 		} else {
@@ -34,7 +19,7 @@ class Account {
 	}
 
 	public synchronized void witdraw() {
-		int money = 1;	//设置取款额度为1元
+		int money = 1;	//设置取款初始额度为1元
 		if (money >= 1 && money < balance) {
 			balance -= money;
 		} else {
--- src/task4/Main.java
+++ src/task4/Main.java
@@ -3,12 +3,18 @@ package task4;
 public class Main {
 	public static void main(String[] args) {
 		
-		Account account = new Account(100);
+		Account account = new Account(100);	// 设置帐户初始额为100元
 		
 		Thread thread = new Thread(new Deposit(account));
 		Thread thread2 = new Thread(new Withdraw(account));
 		
 		thread.start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		thread2.start();
 		
 	}
